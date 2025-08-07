c74b1470cff4e47df4474c7bfeb0143110e7a0e3
jeetmajumder6464 <gabrugaming8250@gmail.com>
2025-08-08 02:43:14 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Bi0GETAL#M-Gwz6YQavVTGSmjrI-CcVYgp1nCAnvgS5iu9Q-rAdA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "9475846199", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
